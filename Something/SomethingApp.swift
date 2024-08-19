fca7e8ba64d8f3356100c06d2ddbd046664b6890
Stefan Walkner <stefan@arkulpa.at>
2024-08-19 16:28:40 +0200

    Initial Commit

---
+++ Something/SomethingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SomethingApp.swift
+//  Something
+//
+//  Created by Stefan Walkner on 19.08.24.
+//
+
+import SwiftUI
+
+@main
+struct SomethingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
