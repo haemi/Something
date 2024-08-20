f3f6c4c18d1a632ff6aebaebf698b0ec02fdfed7
Stefan Walkner <stefan@arkulpa.at>
2024-08-20 08:15:41 +0200

    Initial Commit

---
+++ FooBar Watch App/FooBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  FooBarApp.swift
+//  FooBar Watch App
+//
+//  Created by Stefan Walkner on 19.08.24.
+//
+
+import SwiftUI
+
+@main
+struct FooBar_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
